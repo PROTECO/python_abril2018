f0174d9ea47f002be96aaaef995dbffdbf918d26
Julio César Martínez <31251600+joule7@users.noreply.github.com>
2018-04-16 14:47:28 -0500

    Delete copiaarchivo.py

--- copiaarchivo.py
+++
@@ -1,9 +0,0 @@
-#########################
-##Copiar archivo a otro##
-#########################
-
-origen=open(input("Archivo origen: "),"r+")
-destino=open(input("Archivo destino: "),"w")
-
-for linea in origen.readlines():
-	destino.write(linea)
